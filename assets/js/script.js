ee992364751bce74be10cf65a55d4225bad8f83c
Rontoni <rjjjohansson@gmail.com>
2022-12-19 11:56:37 +0000

    Add function for game start, mouse events and ending placing and swaping turn

--- assets/js/script.js
+++ assets/js/script.js
@@ -21,3 +21,60 @@ const restartButton = document.getElementById('restartButton');
 const winningMessageTextElement = document.getElementById('restartButton');
 let isPlayer_O_Turn = false
 
+// Function for starting the game
+startGame() 
+
+    restartButton.addEventListener('click', startGame);
+
+    function startGame() {
+        isPlayer_O_Turn = false;
+        cellElements.forEach(cell => {
+            cell.classList.remove(PLAYER_X_CLASS);
+            cell.classList.remove(PLAYER_O_CLASS);
+            cell.removeEventListener('click', handleCellClick);
+            cell.addEventListener('click', handleCellClick, { once: true})
+        })
+        setBoardHoverClass ()
+        winningMessageElement.classList.remove('show');
+    }
+
+// Function for mouse click events 
+function handleCellClick(e) {
+    const cell = e.target;
+    const currentClass = isPlayer_O_Turn ? PLAYER_O_CLASS : PLAYER_X_CLASS;
+    placeMark(cell, currentClass);
+    if (checkWin(currentClass)) {
+        endGame(false);
+    } else if (isDraw()) {
+        endGame(true)
+    } else {
+        swapTurns()
+        setBoardHoverClass()
+    }
+}
+
+// Function for ending the game 
+function endGame(draw) {
+    if (draw) {
+        winningMessageTextElement.innerText = "It's a draw!"
+    } else {
+        winningMessageTextElement.innerText = `Player with ${isPlayer_O_Turn ? "O's" : "X's"} wins!`;
+    }
+    winningMessageElement.classList.add('show');
+}
+
+// Function for a drawn game 
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(PLAYER_X_CLASS) || cell.classList.contains(PLAYER_O_CLASS)
+    });
+}
+
+// Functions for placing and swaping turns 
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass);
+}
+function swapTurns() {
+    isPlayer_O_Turn = !isPlayer_O_Turn
+}
+
